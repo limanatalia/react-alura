0b712e953c4c5f15377dc8d80f2106fab8001bc8
Natalia Lima <natalima.al@gmail.com>
2022-08-15 16:07:34 -0300

    reescrevendo codigo

---
+++ ceep/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <section>
+    <form>
+      <input type="text" placeholder="Titulo" />
+      <textarea placeholder="Escreva sua Nota.."/>
+      <button>Criar Nota</button>
+    </form>    
+    </section>
+
+    );
+}
+
+//react -> lib
+//React -> ecossistema
+export default App;
